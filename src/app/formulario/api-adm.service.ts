45873f71c916ed9d9a0448c5d459ab769527927f
Henrique <henrixe80@gmail.com>
2024-07-31 21:24:47 -0400

    Tentativa 1 workflow github

--- src/app/formulario/api-adm.service.ts
+++ src/app/formulario/api-adm.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiAdmService {
-  private baseUrl = 'http://localhost:3101';
+  private baseUrl = environment.baseUrl;
 
   constructor(private http: HttpClient) {}
 
